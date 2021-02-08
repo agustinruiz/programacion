c690b40c58531f360361bbb88c45588e89608924
agustinomarruiz <agustinomarruiz@gmail.com>
2021-02-08 10:32:41 -0300

    Dynamic Programming - Can Construct excercise - Recursive solution optimized with memoization

--- programacion-dinamica/canConstruct.py
+++ programacion-dinamica/canConstruct.py
@@ -19,15 +19,33 @@ def can_construct(target, word_bank):
     return False
 
 
+def can_construct_memoized(target, word_bank, memo):
+    if target in memo:
+        return memo[target]
+    if target == '':
+        return True
+
+    for word in word_bank:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            # Si puedo construir el substring devuelvo True
+            if can_construct_memoized(suffix, word_bank, memo):
+                # Siempre almaceno el target. no el suffix porque cuando retorno el suffix en realidad es el target
+                memo[target] = True
+                return True
+    memo[target] = False
+    return False
+
+
 # True
 print(
-    f"Can construct 'abcdef' ['ab','abc','cd','def','abcd']: {can_construct('abcdef',['ab','abc','cd','def','abcd'])}")
+    f"Can construct 'abcdef' ['ab','abc','cd','def','abcd']: {can_construct_memoized('abcdef',['ab','abc','cd','def','abcd'],{})}")
 # False
 print(
-    f"Can construct 'skateboard' ['bo','rd','ate','t','ska','sk','boar']: {can_construct('skateboard',['bo','rd','ate','t','ska','sk','boar'])}")
+    f"Can construct 'skateboard' ['bo','rd','ate','t','ska','sk','boar']: {can_construct_memoized('skateboard',['bo','rd','ate','t','ska','sk','boar'],{})}")
 # True
 print(
-    f"Can construct 'enterapotentpot' ['a','p','ent','enter','ot','o','t']: {can_construct('enterapotentpot',['a','p','ent','enter','ot','o','t'])}")
+    f"Can construct 'enterapotentpot' ['a','p','ent','enter','ot','o','t']: {can_construct_memoized('enterapotentpot',['a','p','ent','enter','ot','o','t'],{})}")
 # False
 print(
-    f"Can construct 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef' ['e','ee','eee','eeee','eeeee','eeeeee']: {can_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef',['e','ee','eee','eeee','eeeee','eeeeee'])}")
+    f"Can construct 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef' ['e','ee','eee','eeee','eeeee','eeeeee']: {can_construct_memoized('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef',['e','ee','eee','eeee','eeeee','eeeeee'],{})}")
